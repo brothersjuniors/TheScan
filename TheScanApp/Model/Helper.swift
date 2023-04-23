e410f3808c2576e7f164a9d016f9a3d18ba01951
brothersjuniors <101852740+brothersjuniors@users.noreply.github.com>
2023-04-23 14:54:43 +0900

    Add files via upload

---
+++ TheScanApp/Model/Helper.swift
@@ -0,0 +1,26 @@
+//
+//  Helper.swift
+//  ScanApp
+//
+//  Created by 近江伸一 on 2023/04/19.
+//
+
+import Foundation
+import RealmSwift
+
+class Helper{
+    let realm = try! Realm()
+    func saveDate(name: String) {
+        let newItem = Artical()
+        newItem.name = name
+        try! realm.write{
+            realm.add(newItem)
+            
+        }
+    }
+    func deleteItem(item:Artical){
+        try! realm.write(){
+            realm.delete(item)
+        }
+    }
+}
---
+++ TheScanApp/Views/NextViewController.swift
@@ -0,0 +1,83 @@
+//
+//  NextViewController.swift
+//  ScanApp
+//
+//  Created by 近江伸一 on 2023/03/04.
+
+
+import UIKit
+import RealmSwift
+class NextViewController: UIViewController {
+  let realm = try! Realm()
+    var str = ""
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        textView.text = str
+        if textView.text == "" {
+            return
+        } else {
+            
+            showAleart()
+        }
+       
+    }
+
+        
+    
+    
+    @IBAction func backButton(_ sender: UIButton) {
+
+            self.dismiss(animated: true)
+        
+       
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+            self.view.endEditing(true)
+        }
+    func showAleart(){
+        // UIAlertControllerの生成
+         let alert = UIAlertController(title: "スキャンしたデーアを保存しますか？", message: "取得したデータ", preferredStyle: .alert)
+
+         // アクションの生成
+        let yesAction = UIAlertAction(title: "はい", style: .default) { [self] action in
+             Helper().saveDate(name: textView.text )
+            textView.text = ""
+            
+         }
+    
+
+         let noAction = UIAlertAction(title: "いいえ", style: .destructive) { action in
+            
+         }
+
+//         let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel) { action in
+//
+//         }
+
+         // アクションの追加
+         alert.addAction(yesAction)
+         alert.addAction(noAction)
+//         alert.addAction(cancelAction)
+
+         // UIAlertControllerの表示
+         present(alert, animated: true, completion: nil)
+      
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if textView.text == "" {
+            return
+        } else {
+            
+            showAleart()
+        }
+    }
+
+    
+}
